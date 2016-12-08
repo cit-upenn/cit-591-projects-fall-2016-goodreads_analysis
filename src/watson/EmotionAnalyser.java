dd842b9af452ece2a8576b1ec706e36fff27928c
Carson Stack <carsonstack@seas.3514.apng.wireless-pennnet.upenn.edu>
2016-12-08 14:31:19 -0500

    tone analyser now just returns emotions

--- src/watson/EmotionAnalyser.java
+++ src/watson/EmotionAnalyser.java
@@ -1,7 +1,10 @@
 package watson;
 
 import com.ibm.watson.developer_cloud.tone_analyzer.v3.ToneAnalyzer;
+import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.Tone;
 import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.ToneAnalysis;
+import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.ToneOptions;
+import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.ToneOptions.Builder;
 
 public class EmotionAnalyser {
 	private ToneAnalysis tone;
@@ -10,8 +13,14 @@ public class EmotionAnalyser {
 		ToneAnalyzer service = new ToneAnalyzer(ToneAnalyzer.VERSION_DATE_2016_05_19);
 		service.setUsernameAndPassword("92dc16f3-482d-46d0-9f83-4d7d223ad9b1", "vyWzY7PIs37e");
 
+		//set the tone options to only display emotions
+		Tone emotions = Tone.EMOTION;
+		Builder toneBuilder = new Builder();
+		ToneOptions options = toneBuilder.addTone(emotions).build();
+		
+		
 		// Call the service and get the tone
-		tone = service.getTone(text, null).execute();
+		tone = service.getTone(text, options).execute();
 	}
 
 	/**
