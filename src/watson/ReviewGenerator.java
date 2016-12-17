cc6438f0356bf9629c80cfa9112aad819755dd3a
Carson Stack <carsonstack@seas.1004.apng.wireless-pennnet.upenn.edu>
2016-12-17 17:05:19 -0500

    more comments

--- src/watson/ReviewGenerator.java
+++ src/watson/ReviewGenerator.java
@@ -2,8 +2,19 @@ package watson;
 
 import java.util.ArrayList;
 
+/**
+ * This is the ReviewGenerator class.
+ * It creates the reviews for a book.
+ * @author carsonstack
+ *
+ */
 public class ReviewGenerator {
 	
+	/**
+	 * This is the constructor method
+	 * @param content an array of strings with review content
+	 * @param book the book to create reviews for
+	 */
 	public ReviewGenerator(ArrayList<String> content, Book book){
 		for (String each : content ) {
         	Review review = new Review(each);
