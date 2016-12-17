fe062edf66b9cdb7e79765f7573ee7d057280267
dmille <david.milewicz@yale.edu>
2016-12-17 13:15:57 -0500

    added GR review work

---
+++ src/API/GRReviewLinks.java
@@ -0,0 +1,44 @@
+package API;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class GRReviewLinks implements URLBridgeList {
+	private String contentClassName;
+	
+	public GRReviewLinks () {
+		this.contentClassName = "gr_more_link";
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see API.URLBridgeList#getURLBridges(java.lang.String)
+	 */
+	@Override
+	public ArrayList<String> getURLBridges (String sourceURL) throws IOException {
+		
+		Document contentBridge = Jsoup.connect(sourceURL).get();
+		
+		// get content id addresses 
+		Elements bridges = contentBridge.getElementsByClass(this.contentClassName);
+		
+		// make list for the review addresses
+		ArrayList<String> contentURLStrings = new ArrayList<>();
+		
+		// get Content URLs from href tags
+		for (Element elem : bridges) {
+			System.out.println(elem);
+			contentURLStrings.add( elem.attr("abs:href"));
+		}
+		
+		return contentURLStrings;
+		
+		
+	}
+	
+}
