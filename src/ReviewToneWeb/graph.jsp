8a93d6dc2301335ab5f8b9ad4a4eb35140e8f68a
dmille <david.milewicz@yale.edu>
2016-12-17 23:05:27 -0500

    changed graph to jsp

---
+++ src/ReviewToneWeb/graph.jsp
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<head>
+  <title>HTML5 Bar Graph Example</title>
+</head>
+
+<body>
+	<%
+	out.println("Your IP address is " + request.getRemoteAddr());
+	%>
+  <div id="graphs">
+  <div id = graphDiv1></div>
+  </div>
+  <script src="js/barGraph.js"></script>
+  <script>(function () {
+	  var makeGraphs = "";
+	  
+	  
+/* 	  for (var i = 0 ; i < 4; i++) {
+		  makeGraphs += "<div id=\"graphDiv" + i + "\"></div>";
+	  } */
+	  
+	  document.getElementById("graphs").innerHTML = makeGraphs;
+  
+    function createCanvas(divName) {
+      
+      var div = document.getElementById(divName);
+      var canvas = document.createElement('canvas');
+      div.appendChild(canvas);
+      if (typeof G_vmlCanvasManager != 'undefined') {
+        canvas = G_vmlCanvasManager.initElement(canvas);
+      } 
+      var ctx = canvas.getContext("2d");
+      return ctx;
+    }
+	  
+	    var ctx = createCanvas("graphDiv1");
+	    
+	    var graph = new BarGraph(ctx);
+	    graph.maxValue = 100;
+	    graph.margin = 2;
+	    graph.colors = ["#007300", "#ff0000", "#007300", "#ff0000", "#007300"];
+	    graph.xAxisLabelArr = ["Anger", "Disgust", "Fear", "Joy", "Sadness"];
+	    graph.update(<% out.println(request.getParameter("emotions").toString());  %>);
+  
+  
+  
+  }());
+  	</script>
+  
+
+
+</body>
+</html>
+
--- src/Servletin.java
+++ src/Servletin.java
@@ -1,7 +1,15 @@
 import java.io.IOException;
+import java.util.Arrays;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import watson.*;
 
 
 public class Servletin extends  HttpServlet {
@@ -35,29 +43,7 @@ public class Servletin extends  HttpServlet {
 		 
 	 }
 		 
-//		 RequestDispatcher view = request.getRequestDispatcher("tester.html");
-//		 
-//		 view.forward(request, response);
-		 
-		 
-		 
-//		 response.setContentType("text/html");
-//		 
-////		 response.sendRedirect("http://localhost:8080/HelloWorld/test");
-//
-//		 java.io.PrintWriter out = response.getWriter();
-//		 // output your page here
-//		 out.println("<html>");
-//		 out.println("<head>");
-//		 out.println("<title>Servlet</title>");
-//		 
-//		 int x = 6;
-//		 out.println("</head>");
-//		 out.println("<body>");
-//		 out.println("Hello, Java Servlets" + x);
-//		 out.println("</body>");
-//		 out.println("</html>");
-//		 out.close();
+
 	 
 
 
@@ -68,12 +54,15 @@ public class Servletin extends  HttpServlet {
 		
 		
 		
-		 RequestDispatcher view = request.getRequestDispatcher("/graph.html");
+		 RequestDispatcher view = request.getRequestDispatcher("/graph.jsp");
+		 
+		 
+		
+		 double[] values = GRReviewFactory.getReviews((String) request.getAttribute("var"), 5);
+		 
+		 request.setAttribute("emotions", values);
 		 
 		 view.forward(request, response);
-		
-		 
-		 
 		 
 		 
 		 
